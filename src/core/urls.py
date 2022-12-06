84bb34071e3d0052b10faa80299e6f0aa4326efb
Noopur Phalak <noopur@pixeldust.in>
2022-12-06 09:49:50 +0530

    bug fix

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,5 +4,5 @@ from . import views as core_views
 
 urlpatterns = [
     path("posts/", core_views.posts, name="posts"),
-    path("posts/<int:id>/", core_views.post, name="post"),
+    path("posts/<int:post_id>/", core_views.post, name="post"),
 ]
